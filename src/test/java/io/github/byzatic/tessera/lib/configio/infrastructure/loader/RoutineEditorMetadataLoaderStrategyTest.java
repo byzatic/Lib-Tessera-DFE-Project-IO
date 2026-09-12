60ce7367ccbf67bd6a371028044eb331b49c0695
Pavel Frasyn <frasyn.pashka@gmail.com>
2026-09-12 21:13:15 +0300

    update

--- src/test/java/io/github/byzatic/tessera/lib/configio/infrastructure/loader/RoutineEditorMetadataLoaderStrategyTest.java
+++ src/test/java/io/github/byzatic/tessera/lib/configio/infrastructure/loader/RoutineEditorMetadataLoaderStrategyTest.java
@@ -8,8 +8,6 @@ import io.github.byzatic.tessera.lib.configio.support.TestProjectFixture;
 import org.junit.Test;
 
 import java.util.List;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -86,28 +84,4 @@ public class RoutineEditorMetadataLoaderStrategyTest {
         }
     }
 
-    @Test
-    public void shouldLoadProviderCompiledAgainstPreviousUnifiedApi() throws Exception {
-        Path directory = Files.createTempDirectory("legacy-unified-routine-");
-        Path jar = directory.resolve("legacy-unified-routine-provider.jar");
-        try (var input = getClass().getResourceAsStream(
-                "/legacy-unified-routine-provider.jar"
-        )) {
-            Files.copy(java.util.Objects.requireNonNull(input), jar);
-        }
-        try (RoutineEditorMetadataLoaderInterface loader =
-                     RoutineEditorMetadataLoaderFactory.create(directory, (ClassLoader) null)) {
-            RoutineEditorMetadataDataObject metadata = loader.getAvailableMetadata().get(0);
-
-            assertEquals("LegacyUnifiedRoutine", metadata.getRoutineId());
-            assertEquals(List.of(), metadata.getDescriptor().getRoutineWidgetIds());
-            assertEquals(List.of(), metadata.getDescriptor().getEnvironment());
-            assertEquals(List.of(), metadata.getDescriptor().getConfigurationFiles());
-            assertEquals(List.of("FuncENV"), metadata.getDescriptor().getFunctions()
-                    .get(0).getBduiWidgetIds());
-        } finally {
-            Files.deleteIfExists(jar);
-            Files.deleteIfExists(directory);
-        }
-    }
 }
